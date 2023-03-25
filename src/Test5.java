7a026ca8acecaedb24afcba650a95163cc78fe2e
sonsungjun <qqeerr123@naver.com>
2023-03-26 02:42:24 +0900

    변경테스트

--- src/Test5.java
+++ src/Test5.java
@@ -2,4 +2,9 @@ import java.io.*;
 import java.util.*;
 
 public class Test5 {
+    public static void main(String args[])throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+    }
 }
