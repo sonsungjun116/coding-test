0886ba1d50c6b4128a632bd5130ce50f2845dcbb
sonsungjun <qqeerr123@naver.com>
2023-03-27 23:38:15 +0900

    꿀 따기

---
+++ src/test_20230327.java
@@ -0,0 +1,14 @@
+
+import java.io.*;
+import java.util.*;
+
+public class test_20230327 {
+    public static void main(String args[])throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        //벌꿀담기
+    }
+
+}
+기
