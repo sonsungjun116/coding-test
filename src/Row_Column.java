f38cff2c82838749cd62b86c49670438db3cbe2d
sonsungjun <qqeerr123@naver.com>
2023-04-09 19:30:01 +0900

    그리디 알고리즘 행렬 계산 2023.04.09

--- src/Row_Column.java
+++ src/Row_Column.java
@@ -8,11 +8,11 @@ public class Row_Column {
         StringTokenizer st;
 
         //행렬
-        // 행렬값 첫번째 줄은 row수 두번쨰 수는 column수이다.
+        // 행렬값 첫번째 줄의 첫번째수는 row수 두번쨰 수는 column수이다.
         // 행렬값은 0과 1로만 이루어져 있고 행렬은 a,b 두가지가 존재한다.
         // 목적은 행렬 a를 b의 형태로 변형 시키는 것 이다.
-        // 연산의 기준은 3x3이다. -> row와 column수가 3을 넘지 않는다면 답은 자동으로 -1이 출력이 된다.
-        // -> 즉 column수는 4이상부터는
+        // 연산의 기준은 반드시 3x3이다. -> row와 column수가 3을 넘지 않는다면 답은 자동으로 -1이 출력이 된다.
+        // 또한 연산에 해당되면 무조건 값은 0 -> 1로 1 ->0으로 변환된다.
         st = new StringTokenizer(br.readLine()," ");
 
         int R = Integer.parseInt(st.nextToken());
@@ -34,7 +34,21 @@ public class Row_Column {
                     for (int j = 0; j < test.length(); j++) array_b[k][j] = test.charAt(j) - 48;
                 }
             }
-//            System.out.println("array = "+ Arrays.toString(array));
+        }
+
+        // 검사 방식으로는 2차원 배열을 한줄씩 검사하고 만약 B와 다르다면 연산을 수행하는 것으로 진행한다
+        // 연산 수행시 cnt 증가
+        // 우선 column수는 3으로 가정하고 진행해본다.
+
+        int CNT = 0;
+
+        for(int i = 0; i < R; i++){
+            for(int j = 0; j < C;j++){
+                if(array_a[R][C] != array_b[R][C]){
+                    //행렬 A와 B의 R행 값이 다르므로 R행은 연산의 적용 대상이다.
+                }
+            }
+
         }
 
         System.out.println("array_a = "+Arrays.deepToString(array_a));
