f888ac74c62a72c0083c8db0857372c9b589d8d9
sonsungjun <qqeerr123@naver.com>
2023-05-31 00:05:13 +0900

    2138 전구와 스위치 그리디 알고리즘 2023.05.16

---
+++ src/switch_and_bulb2.java
@@ -0,0 +1,24 @@
+import java.util.*;
+import java.io.*;
+
+public class switch_and_bulb2 {
+
+    public static void main(String args[])throws IOException {
+
+        // 1. 첫번째 전구를 킨 경우와 키지 않은 경우로 구분 지어서 구현
+
+        // 2. 2 ~ n-1 인덱스를 순회하여 값을 찾는다
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
