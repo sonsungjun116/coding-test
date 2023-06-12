4a44cfb0658f6ea30f0c6ce2822e0eac2b62b472
sonsungjun <qqeerr123@naver.com>
2023-06-13 01:27:53 +0900

    1700 멀티 스케줄링 그리디 알고리즘 2023.05.16

--- src/multitap.java
+++ src/multitap.java
@@ -8,8 +8,8 @@ public class multitap{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
 
-        int N;
-        int K;
+        int N = 0;
+        int K = 0;
 
         // 1. 콘센트의 수 인 N개의 크기 배열에 K개의 숫자 번호를 넣고 N+1번째 인덱스의 값이 배열에 있는지 확인
         // -> 해당 배열안에 N+1 인덱스의 값이 존재한다면 pass
@@ -19,18 +19,29 @@ public class multitap{
         // 3. 교체 시 다음 번째 인덱스부터 N개의 배열중 현재 배열과 같은것이 없는것으로 교체
         // -> 만약 모두 다르거나 몇가지가 다르다면 아무거나 교체
 
-        for(int i = 0; i < 2; i++) {
+        for(int i = 0; i < 1; i++) {
             st = new StringTokenizer(br.readLine(), " ");
             N = Integer.parseInt(st.nextToken());
             K = Integer.parseInt(st.nextToken());
-            int multitap_hole[] = new int[N];
-            int elect_product[] = new int[K];
         }
 
-            for(int i = 0 ; i < K; i++){
-                elect_product[i] = Integer.parseInt(st.nextToken());
-            }
+        int multitap_hole[] = new int[N];
+        int elect_product[] = new int[K];
 
+        System.out.println("N = " + N);
+        System.out.println("K = " + K);
+
+        st = new StringTokenizer(br.readLine()," ");
+        int s_count = st.countTokens();
+        for(int i =0; i < K; i++){
+            elect_product[i] = Integer.parseInt(st.nextToken());
+        }
+
+//            for(int i = 0 ; i < K; i++){
+//                System.out.println("i = "+i);
+//                st = new StringTokenizer(br.readLine(), " ");
+//                elect_product[i] = Integer.parseInt(st.nextToken());
+//            }
             System.out.println(Arrays.toString(elect_product));
 
       //  st = new StringTokenizer(br.readLine()," ");
@@ -39,8 +50,8 @@ public class multitap{
 //        while(st.hasMoreTokens()){
 //            System.out.println("st.nextToken() : " + st.nextToken());
 //        }
-        System.out.println("N = " + N);
-        System.out.println("K = " + K);
+
+
 
     }
 }
