76d6370293caa28f66d8d70677a20ab53a6b0f28
sonsungjun <qqeerr123@naver.com>
2023-09-03 17:04:56 +0900

    완전 탐색 알고리즘 2023.08.16 순열

--- src/seven_dwarf.java
+++ src/seven_dwarf.java
@@ -12,8 +12,6 @@ public class seven_dwarf {
         StringTokenizer st;
 
         int sum = 0;
-        int difference;
-        int compare = 0;
         // 백설공주 난쟁이 문제
 
         // 완전 탐색 알고리즘
@@ -24,84 +22,47 @@ public class seven_dwarf {
         // 9가지의 숫자중 7가지를 선별하여 그들의 합이 100
 
         // 접근 방식
-        // 1. 9개의 입력값들을 오름차순으로 정렬하고 앞 배열부터 7개로 잘라서 배열에 넣고 검사를 시작
-        // 2. 경우의수
-        // 2-1 100보다 작은 경우
-            // 100에서 합계 값을 빼고 어떠한 배열을 바꾸면 그 부족한 숫자만큼 채워지는지 확인
-        // 2-2 100보다 큰 경우
-            // 합계에서 100을 빼고 어떠한 배열을 바꾸면 그 넘치는 숫자만큼 차감 할 수 있는지 확인
+        // 9가지의 값을 전부 더한 후 이중 for문을 돌려 두개의 인덱스를 순회하면서 뺴보고 그 값이 100이 나오는 경우 배열값 출력
 
 
         int input_array[] = new int[9]; // 처음 값을 입력받는 배열
-        int seven_dwarf_array[] = new int[7];   // 9개의 입력값중 7개의 값을 선정
-        int remain_array[] = new int[2]; // 나머지 선정되지 않은 2개의 값
+//        int seven_dwarf_array[] = new int[7];   // 9개의 입력값중 7개의 값을 선정
+        //int remain_array[] = new int[2]; // 나머지 선정되지 않은 2개의 값
 
         for (int i = 0; i < input_array.length; i++) {
             input_array[i] = Integer.parseInt(br.readLine());
         }
-        Arrays.sort(input_array);
 
         System.out.println("input_array = " + Arrays.toString(input_array));
-
-        for(int i = 0; i < input_array.length; i++) {
-            if(i < 7) {
-                seven_dwarf_array[i] = input_array[i];
-            }else {
-                remain_array[i-7] = input_array[i];
-            }
-        }
-
-        System.out.println("seven_dwarf_array = " + Arrays.toString(seven_dwarf_array));
-        System.out.println("remain_array = " + Arrays.toString(remain_array));
-
-        sum = Arrays.stream(seven_dwarf_array).sum();
-
+        sum = Arrays.stream(input_array).sum();
         System.out.println("sum = " + sum);
 
-        loop:
-        if(sum == 100){
-            System.out.println("answer = " + Arrays.toString(seven_dwarf_array));
-        }else if (sum < 100){ // 합계가 100이하라면
-            difference = 100 - sum;
-            // remain_array 배열의 첫번째 요소 교환
-            for(int i = 0; i < seven_dwarf_array.length; i++){
-                int sum_value = (sum - seven_dwarf_array[i]) + remain_array[0];
-                if(sum_value == 100) {
-                    seven_dwarf_array[i] = remain_array[0];
-                    System.out.println("answer = " + Arrays.toString(seven_dwarf_array));
-                    break loop;
+        for(int i = 0; i < input_array.length-1; i++){
+            for(int j = i+1; j < input_array.length; j++){
+                if(sum - input_array[i] - input_array[j] == 100){
+                    System.out.println("sum = > " + sum);
+                    System.out.println("answer1 array = " + input_array[i]);
+                    System.out.println("answer2 array = " + input_array[j]);
+                    System.out.println("sum answer = " + ((sum - input_array[i]) - input_array[j]));
+                    int minus1 = input_array[i];
+                    int minus2 = input_array[j];
+
+                    Arrays.sort(input_array);
+                    System.out.println("minus1 = " + minus1);
+                    System.out.println("minus2 = " + minus2);
+                    for(int k = 0; k < input_array.length; k++){
+                        if(input_array[k] == minus1 || input_array[k] == minus2)continue;
+                            System.out.println("answer -> " + input_array[k]);
+
+
+                    }
+
                 }
             }
-            // remain_array 배열의 두번째 요소 교환
-            for(int i = 0; i < seven_dwarf_array.length; i++){
-                int sum_value = (sum - seven_dwarf_array[i]) + remain_array[1];
-                if(sum_value == 100) {
-                    seven_dwarf_array[i] = remain_array[1];
-                    System.out.println("answer = " + Arrays.toString(seven_dwarf_array));
-                    break loop;
-                }
-            }
-            // 두개의 remain_array 배열의 요소를 삽입하여 도출
 
-            // 초기 교환 인덱스
-            int index1 = 0;
-            int index2 = 1;
-            // 비교 배열 생성
-            int compare_array[] = new int[7];
-            // seven_dwarf_array 배열값 복사
-            System.arraycopy(seven_dwarf_array,0,compare_array,0,seven_dwarf_array.length);
-            System.out.println("compare array = " + Arrays.toString(compare_array));
-            for(int i = 0; i < seven_dwarf_array.length; i++){
-
-
-
-            }
-
-
-
-        }else if (sum > 100){ //합계가 100 이상이라면
 
         }
+
 
 
 
